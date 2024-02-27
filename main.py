739a9807dc97730f3acdcf36eeb90030de5aaa8c
Ecl1ps0 <daniildan74@gmail.com>
2024-02-27 19:38:34 +0600

    chore: remove some comments

--- main.py
+++ main.py
@@ -10,21 +10,7 @@ from bots import OutlookBot, LinkedInBot, TwitterBot
 def main():
     client = APIClient(configs.access_token_payload)
 
-    # proxies = client.get_proxies(configs.us_proxy_params)
-    # for proxy in proxies:
-    #     email = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) + '@1secmail.com'
-    #
-    #     bot = TwitterBot(client, proxy=proxy)
-    #     bot.create_new_token(email)
-
-    # static_proxies = client.get_proxies(configs.us_proxy_params)
-    #
-    # for proxy in static_proxies:
-    #     email = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) + '@1secmail.com'
-    #
-    #     bot = OutlookBot(client, proxy=proxy)
-    #     bot.create_new_token(email)
-
+    # LinkedIn login
     tokens = client.get_tokens(configs.linkedin_tokens_params)
     static_proxies = client.get_proxies(configs.us_proxy_params)
 
@@ -32,10 +18,15 @@ def main():
         bot = LinkedInBot(client, token, proxy)
         bot.update_token_to_active()
 
+    # #LinkedIn registration
     # dynamic_proxies = client.get_proxies(params=configs.dynamic_proxy_params)
     #
     # for proxy in dynamic_proxies:
     #     email = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) + '@1secmail.com'
+    #
+    #     if not proxy:
+    #         dynamic_proxies = client.get_proxies(params=configs.dynamic_proxy_params)
+    #         continue
     #
     #     bot = LinkedInBot(client, proxy=proxy)
     #     bot.create_new_token(email)
